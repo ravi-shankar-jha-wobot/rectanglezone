cb30ea50d0c80af9bd6d2eaf0d9f9a91955e3585
Ravi <ravi.jha@wobot.ai>
2025-08-27 18:58:15 +0530

    onchange test#3

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,39 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib/**/*.ts", "lib/**/*.tsx"] })],
+  css: {
+    postcss: undefined,
+  },
   build: {
+    copyPublicDir: true,
     lib: {
-      entry: resolve(__dirname, "src/lib/index.ts"),
-      name: "RectangleZone",
-      fileName: "index",
-      formats: ["es", "cjs"],
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "operational-dashboard",
+      fileName: "operational-dashboard",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+          "react-router-dom": "react-router-dom",
         },
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name]-[hash].js",
       },
     },
-    sourcemap: true,
-    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./lib"),
+    },
   },
 });
